4639b0c7aaedd4fca8cb2d4c7b2e0854c07f47f6
James <james.jcarline@gmail.com>
2022-12-12 22:53:54 +0000

    Implement game end logic framework

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -28,15 +28,48 @@ class GameBoard
         @column = column.to_i - 1
     end
     def place_symbol(symbol)
+
         puts "Enter the row you wish to select (1, 2 or 3)"
         self.row_select(gets)
+
         puts "Enter the column you wish to select (1, 2 or 3)"
         self.column_select(gets)
+
         if @board_array[@row][@column] == 'X' || @board_array[@row][@column] == 'O'
             puts "That space is taken. Please select an empty space."
             self.place_symbol(symbol)
         else
             @board_array[@row][@column] = symbol
+        end
+    end
+    def check_winner
+        check_1 = []
+        check_2 = []
+        check_3 = []
+        for i in 0..2 do
+            for j in 0..2 do
+                check_1 << @board_array[i][j]
+                check_2 << @board_array[j][i]
+            end
+            
+            unless check_1[0] == '#' || check_2[0] == '#'    
+                if check_1[0] == check_1[1] && check_1[0] == check_1[2]
+                    if check_1[0] == 'X'
+                        puts "#{player1_name} wins!"
+                    elsif check_1[0] == "O"
+                        puts "#{player2_name} wins!"
+                    end
+                elsif check_2[0] == check_2[1] && check_2[0] == check_2[2]
+                    if check_2[0] == 'X'
+                        puts "#{player1_name} wins!"
+                    elsif check_2[0] == "O"
+                        puts "#{player2_name} wins!"
+                    end
+                else
+                    check_1.clear
+                    check_2.clear
+                end
+            end
         end
     end
 end
@@ -50,10 +83,12 @@ for i in 0..1 do
     #sets player symbol
     player_symbol = player1_turn ? 'X' : 'O'
     puts "#{player1_turn ? player1_name.chomp : player2_name.chomp}, it's your turn!"
-    # get player input and place the player's symbol in the desired location
+    # call the place_symbol method on the board instance
     board.place_symbol(player_symbol)
     #output board state
     board.display_board
+    #check for a winner
+    board.check_winner
     #change player turn and player symbol
     player1_turn ? player1_turn = false : player1_turn = true
 end
