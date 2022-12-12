aea3c800baa4d9032b7047a97bccc9e40ec7d1f5
James <james.jcarline@gmail.com>
2022-12-12 15:13:29 +0000

    Implement basic interactivity
    I have added some methods to the GameBoard class that collect input from the player and use that
    to place the desired symbol. First the game asks for a row, and passes it to the row_select method.
    Next, it asks for a column and passes it to the column select method. Finally the place_symbol
    method is called and the active player's symbol is placed in the selected location.

--- tic-tac-toe.rb
+++ tic-tac-toe.rb
@@ -3,30 +3,35 @@ require 'pry-byebug'
 #Variables
 game_playing = true
 player1_turn = true
-player_symbol = player1_turn ? 'X' : 'O'
 
 #Classes
 class Player
     def initialize(name)
         @name = name
     end
-    def test
-        puts "Hi, I'm #{@name}"
-    end
     def name
         return @name.chomp
     end
+    
 end
 
 class GameBoard
     def initialize
-        @board_array = [['___'],['___'],['___']]
+        @board_array = [['#','#','#'],['#','#','#'],['#','#','#']]
+        @row = 0
+        @column = 0
     end
     def display_board
-        puts @board_array
+        puts @board_array[0][0] + @board_array[0][1] + @board_array[0][2]
     end
-    def player_move(row, column)
-        
+    def row_select(row)
+        @row = row.to_i - 1
+    end
+    def column_select(column)
+        @column = column.to_i - 1
+    end
+    def place_symbol(symbol)
+        @board_array[@row][@column] = symbol
     end
 end
 
@@ -39,7 +44,17 @@ player2 = Player.new(gets)
 
 #Game Loop
 #while game_playing == true do
-puts "#{player1_turn ? player1.name : player2.name}, it's your turn! Enter the row you wish to select (A, B or C)"
-
-    
+    #sets player symbol
+    player_symbol = player1_turn ? 'X' : 'O'
+    #get player input
+    puts "#{player1_turn ? player1.name : player2.name}, it's your turn! Enter the row you wish to select (1, 2 or 3)"
+    board.row_select(gets)
+    puts "Now enter the column you want to select (1, 2 or 3)"
+    board.column_select(gets)
+    #place the player's symbol in the desired location
+    board.place_symbol(player_symbol)
+    #output board state
+    board.display_board
+    #change player turn and player symbol
+    player1_turn ? false : true
 #end
